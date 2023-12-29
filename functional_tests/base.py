14ced8e683ee096b0c22ad6013a856bc6a4a2ca6
miraclehand <miraclehand@naver.com>
2023-12-29 18:04:54 +0900

    Moved Fts into their own individual files

---
+++ functional_tests/base.py
@@ -0,0 +1,43 @@
+import os
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+MAX_WAIT = 5
+
+class FunctionalTest(StaticLiveServerTestCase):
+    def setUp(self):
+        service = Service('/usr/bin/chromedriver')
+        chromeOptions = webdriver.ChromeOptions()
+        chromeOptions.add_argument('headless')
+        self.browser = webdriver.Chrome(service=service, options=chromeOptions)
+
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return;
+            except (AssertionError, WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
